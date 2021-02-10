7b38b8454198d101acbb7b5d8a53433d00cbe976
BenKF <67444810+KadirFiratFTW@users.noreply.github.com>
2021-02-10 19:28:42 +0300

    Update server.mjs
    Added OnRoomCount function for getRoomCount event.
    Added OnServerCreated function for onServerCreated event.

--- server/mw_menu/server.mjs
+++ server/mw_menu/server.mjs
@@ -1,20 +1,20 @@
 import * as alt from "alt"
 
 let Rooms = [];
+alt.on("onServerCreated", OnServerCreated)
+alt.onClient("getRoomCount", OnRoomCount)
+alt.onClient("startSearch", SearchStatus)
 
-alt.onClient("getRoomCount", (player) => {
+function (player) {
     alt.emitClient(player, "roomCount", Rooms.length)
-})
+}
 
-alt.on("onServerCreated", (roomID) => {
+function OnServerCreated(roomID){
     if (roomID) {
         Rooms.push(roomID);
     }
     alt.emitClient(null, "roomCount", Rooms.length)
-})
-
-alt.onClient("startSearch", SearchStatus)
-
+}
 
 function SearchStatus(player, status, map, loadout) {
     player.setSyncedMeta("searching", status)
