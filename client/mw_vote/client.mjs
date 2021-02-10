bdb364a405e7439c12e33f82b9253d385e9eb594
BenKF <67444810+KadirFiratFTW@users.noreply.github.com>
2021-02-10 18:44:30 +0300

    Update client.mjs
    - function listen key events is unnecessary.
    - added OnVote, OnVoteEnd, StartVote, StartVoteServer functions.
    - deleted some unnecessary spaces..

--- client/mw_vote/client.mjs
+++ client/mw_vote/client.mjs
@@ -2,47 +2,44 @@ import * as alt from "alt";
 
 const URL = "http://resource/client/mw_vote/html/index.html";
 let View = new alt.WebView(URL);
-
 let Voted = false;
-
 let showCursor = false;
 let gameControls = true;
 let isFocus = false;
-View.on("startVote", (id) => {
 
-    alt.emitServer("vote:start", id)
-
-})
+View.on("startVote", StartVoteServer)
 
 View.on("takeControls", takeControls);
 
-alt.onServer("start:vote", (voter, target) => {
+alt.onServer("start:vote", StarVote)
+
+alt.onServer("onVoteEnd", OnVoteEnd)
+
+alt.onServer("onVote", OnVote)
+
+function StartVoteServer(id){
+
+    alt.emitServer("vote:start", id)
+
+}
+
+function StartVote(voter, target){
     Voted = false;
     View.emit("voteStarted", voter, target)
-})
+}
 
-
-alt.onServer("onVoteEnd", () => {
-
+function OnVoteEnd(){
     View.emit("voteEnd");
+}
 
-})
-
-alt.onServer("onVote", (voteType, voteCount) => {
-
+function OnVote(voteType, voteCount){
     if (View) {
-
         View.emit("voteCount", voteType, voteCount);
-
     }
+}
 
-})
-
-
-ListenKeyEvents();
 
 function takeControls() {
-
     showCursor = !showCursor
     gameControls = !gameControls
     isFocus = !isFocus
@@ -55,43 +52,29 @@ function takeControls() {
     } else {
         View.unfocus();
     }
-
 }
 
-function ListenKeyEvents() {
-
-    alt.on("keyup", KeyEvent)
-
-}
+alt.on("keyup", KeyEvent)
 
 function KeyEvent(key) {
-
     if (key == 116) {
         VoteMenu();
     }
-
     if (Voted) { return; }
     if (key == 112) {
 
         Voted = true
         alt.emitServer("vote", true);
     }
-
     if (key == 113) {
         Voted = true
         alt.emitServer("vote", false);
     }
-
 }
 
 function VoteMenu() {
-
     const Players = alt.Player.all.filter(P => P.scriptID !== alt.Player.local.scriptID && P.getSyncedMeta("Team") == alt.Player.local.getSyncedMeta("Team")).map(P => {
-
         return { 'name': P.getSyncedMeta("UserName"), 'id': P.id }
-
     })
-
     View.emit("vote:playerList", Players)
-
 }
