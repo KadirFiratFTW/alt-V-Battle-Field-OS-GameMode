bfa882eb5dd15395ddfbd73d311b385aa8d113da
BenKF <67444810+KadirFiratFTW@users.noreply.github.com>
2021-02-10 18:57:07 +0300

    Update client.mjs
    Added OnLobbyStart function to lobby:start event.
    Added OnKill function to onKill event
    Added OnKillLog function to onKillLog event
    Added OnLobbyEnded function to lobby:Ended event
    Added OnPoint function to showPoint event.
    
    Added PlaySound function for clean code.  (bruh)
    
    Added OnTick function for everyTick event. 
    
    Added PointQueTick function for PointQue processor. 
    
    Removing spaces..

--- client/mw_manager/client.mjs
+++ client/mw_manager/client.mjs
@@ -3,88 +3,79 @@ import * as native from "natives"
 
 const URL = "http://resource/client/mw_manager/html/index.html"
 let View = null;
-
 const Rels = {
     'Team1': native.addRelationshipGroup("Team1"),
     'Team2': native.addRelationshipGroup("Team2")
 }
-
 let Interval = null;
+let PointQue = [];
 
-alt.onServer("lobby:start", () => {
+alt.onServer("lobby:start", OnLobbyStart)
+alt.onServer("onKill", OnKill)
+alt.onServer("onKillLog", OnKillLog)
+alt.onServer("lobby:Ended", OnLobbyEnded)
+alt.onServer("showPoint", OnPoint)
+alt.everyTick(OnTick)
 
+function OnLobbyStart(){
     View = new alt.WebView(URL);
-
     Interval = alt.everyTick(() => {
-
         alt.Player.all.filter(P => P.getSyncedMeta("Team") && P.getSyncedMeta("Team") == alt.Player.local.getSyncedMeta("Team")).forEach(P => {
-
             native.setPedRelationshipGroupHash(P.scriptID, Rels['Team' + alt.Player.local.getSyncedMeta("Team")][1]);
             native.setPedRelationshipGroupDefaultHash(P.scriptID, Rels['Team' + alt.Player.local.getSyncedMeta("Team")][1]);
             native.setEntityCanBeDamagedByRelationshipGroup(P.scriptID, false, Rels['Team' + alt.Player.local.getSyncedMeta("Team")][1]);
         })
-
         native.setRelationshipBetweenGroups(0, Rels['Team2'][1], Rels['Team1'][1])
         native.setRelationshipBetweenGroups(0, Rels['Team2'][1], Rels['Team2'][1])
         native.setRelationshipBetweenGroups(0, Rels['Team1'][1], Rels['Team2'][1])
         native.setRelationshipBetweenGroups(0, Rels['Team1'][1], Rels['Team1'][1])
-
         native.setRelationshipBetweenGroups(0, Rels['Team1'][1], alt.hash("PLAYER"))
         native.setRelationshipBetweenGroups(0, alt.hash("PLAYER"), Rels['Team1'][1])
-
         native.setRelationshipBetweenGroups(0, Rels['Team2'][1], alt.hash("PLAYER"))
         native.setRelationshipBetweenGroups(0, alt.hash("PLAYER"), Rels['Team2'][1])
-
-
         native.setPedConfigFlag(alt.Player.local.scriptID, 184, false)
     })
-})
+}
 
-
-alt.onServer("onKill", (team1, team2) => {
-
+function OnKill(team1, team2){
     if (!View) { return; }
     View.emit("killCounts", team1, team2)
+}
 
-})
-
-
-alt.onServer("onKillLog", (killerName, victimName, killerTeam, victimTeam, weapon, isHeadshot) => {
-
+function OnKillLog(killerName, victimName, killerTeam, victimTeam, weapon, isHeadshot){
     if (!View) { return; }
     View.emit("killLog", killerName, victimName, killerTeam, victimTeam, weapon, isHeadshot)
+}
 
-})
-
-
-alt.onServer("lobby:Ended", (winner) => {
+function PlaySound(winner){
     if (winner == alt.Player.local.getSyncedMeta("Team")) {
         native.playSoundFrontend(-1, "BASE_JUMP_PASSED", "HUD_AWARDS", false)
-    } else {
-        native.playSoundFrontend(-1, "Bed", "WastedSounds", false)
+        return;
     }
-    View.emit("ended", winner, alt.Player.local.getSyncedMeta("Team"));
+    native.playSoundFrontend(-1, "Bed", "WastedSounds", false)
+}
 
+function OnLobbyEnded(winner){
+    PlaySound(winner)
+    View.emit("ended", winner, alt.Player.local.getSyncedMeta("Team"));
     alt.toggleGameControls(false);
     native.triggerScreenblurFadeIn(2500)
-
     alt.setTimeout(() => {
-
         ReturnMainMenu();
         native.triggerScreenblurFadeOut(2500)
-
     }, 15000);
+}
 
-})
-
-let PointQue = [];
-
-alt.onServer("showPoint", (point, pos) => {
+function OnPoint(point, pos){
     PointQue.push({ 'point': point, 'pos': { ...pos }, 'clearTime': Date.now() + 1200, 'sended': false });
-})
+}
 
-alt.everyTick(() => {
-    if (!PointQue.length) { return; }
+function OnTick(){
+    PointQueTick();
+}
+
+function PointQueTick(){
+ if (!PointQue.length) { return; }
     PointQue = PointQue.filter(point => point.clearTime > Date.now());
     for (let P of PointQue) {
         if (Date.now() > P.clearTime) {
@@ -98,19 +89,16 @@ alt.everyTick(() => {
                 P.sended = true;
             }
         }
-    }
-})
+    }   
+}
 
 function ReturnMainMenu() {
     alt.emit("lobby:goMenu")
     View.destroy();
     View = null;
-
 }
 
-
 function Text3D(x, y, z, text) {
-
     const [bol, _x, _y] = native.getScreenCoordFromWorldCoord(x, y, z);
     if (bol) {
         native.setTextScale(0.40, 0.40)
@@ -118,12 +106,9 @@ function Text3D(x, y, z, text) {
         native.setTextProportional(1)
         native.setTextColour(255, 255, 255, 215)
         native.setTextCentre(true)
-
         native.beginTextCommandDisplayText("STRING");
         native.addTextComponentSubstringPlayerName(`~y~${text} `);
         native.endTextCommandDisplayText(_x, _y);
-
-
         let factor = text.length / 250
         native.drawRect(_x, _y + 0.0150, 0.0 + factor, 0.035, 41, 11, 41, 100)
     }
